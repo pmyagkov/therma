a7d4df7dd5a609ec81ca57fa4864ba712a26ebb1
Pavel Myagkov <myagkov@uchi.ru>
2021-12-09 00:37:12 +0300

    Generate pseudo areas in pipe data

--- pipe-out.py
+++ pipe-out.py
@@ -3,11 +3,31 @@
 import time
 import sys
 import numpy as np
+import math
 
 upper = 24 * 32
-nums = np.arange(0, upper)# np.zeros((upper,))
+nums = np.zeros((24 * 32,))
+nums = np.reshape(nums, (24,32))
+
+# не забудь перевернуть стороны
+# np.fliplr(data_array)
+
+temp = 20
+for i in range(3, 6):
+  for j in range(3, 6):
+    nums[i][j] = temp
+  temp = temp + 2
+
+temp = 30
+for i in range(10, 19):
+  for j in range(10, 19):
+    nums[i][j] = temp
+  temp = temp + math.copysign(2, 14 - i)
+
+nums = np.reshape(nums, (nums.shape[0] * nums.shape[1],))
+
+# print(nums)
 while True:
-  nums = np.random.rand(upper) * 20
   sys.stdout.write(" ".join(np.char.mod('%03d', nums)) + '\n')
   sys.stdout.flush()
   time.sleep(1)
